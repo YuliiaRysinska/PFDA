03d4b350c530e91053d2c398432abbf6f43bda17
Админ <iuy.nickolaencko@gmail.com>
2024-09-30 21:53:20 +0100

    json from web

---
+++ my_work/week1.py
@@ -0,0 +1,11 @@
+import requests
+url ="https://api.coindesk.com/v1/bpi/currentprice.json"
+response = requests.get(url)
+data = response.json()
+print(data)
+print("----------")
+print(data['bpi']['EUR']['rate_float'])
+
+
+
+
